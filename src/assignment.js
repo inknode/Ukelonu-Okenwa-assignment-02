06f8a63a7719cb259835af6ee6321dede4a19dcd
Inknode <inknode@test.com>
2022-07-07 18:42:17 +0100

    Challenge solution for task 1 and 3
    Task 1: sumOfNumbers
    Task 2: celsiusToFahrenheit

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    for(let i = 0; i < arrayOfNumbers.length; i++){
+        sum += arrayOfNumbers[i];
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -47,9 +51,13 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+    let fahrenheit = [];
+    arrayOfNumbers.forEach((arrayOfNumber) => {
+    fahrenheit.push(Number.parseInt((9 / 5) * arrayOfNumber + 32));
+    });
+    return fahrenheit; 
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
